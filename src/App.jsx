cc3b3716dc3d35b76e6f484c4c5b02a9a5c16f4c
Kishor B K <kishorbkgowda36@gmail.com>
2023-10-26 17:58:31 +0530

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+
+function App() {
+
+
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>  
+    </div>
+    
+  )
+}
+
+export default App
